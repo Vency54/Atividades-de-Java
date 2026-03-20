83d0862482c4af6418263c79cae7fd08dc226c0b
Rayssa Silva de Oliveira <143881380+RayssaOlv@users.noreply.github.com>
2026-03-20 20:48:46 -0300

    Update Tp01Ex15.java

--- Tp01Ex15.java
+++ Tp01Ex15.java
@@ -3,14 +3,14 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package tp01x15;
+package tp01Ex15;
 import java.util.*;
 
 /**
  *
  * @author aluno
  */
-public class Tp01x15 {
+public class Tp01Ex15 {
 
     /*Nomes: João Victor Lima Venceslau e Rayssa Silva de Oliveira 
    15. Entrar via teclado com o valor da cotação do dólar e uma certa quantidade de
