5eff0fac3d845564a9e055d7298ae8fea8210efe
Vency54 <jv028venceslau@gmail.com>
2026-03-24 00:44:29 -0300

    Add input validation for geometric mean calculation

--- TP01Ex07.java
+++ TP01Ex07.java
@@ -1,39 +1,70 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package tp01ex07;
+
 import java.util.Scanner;
 
-/**
- *
- * @author aluno
- */
+
 public class TP01Ex07 {
 
-    /**
-     * @param args the command line arguments
-     */
     /*Nomes: João Victor Lima Venceslau e Rayssa Silva de Oliveira 
+    
      7. Calcular e exibir a média geométrica de dois valores quaisquer que serão
       digitados.*/
+    
     public static void main(String[] args) {
-        // TODO code application logic here
-         Scanner scanner = new Scanner(System.in);
+
+        Scanner scanner = new Scanner(System.in);
         double num1;
         double num2;
         double media;
         
+             
+        while (true) {
         System.out.print("Digite o primeiro número: ");
-        num1 = scanner.nextDouble();
 
+            if (scanner.hasNextDouble()) {
+        num1 = scanner.nextDouble();
+                System.out.println();
+                if (num1 > 0) {
+                    break;
+                } else {
+                
+                    System.out.println("Erro! Apenas valores maiores que 0.");
+                    System.out.println();
+                }
+            } else {
+                                System.out.println();   
+                System.out.println("Erro! Apenas números");
+                                System.out.println();
+                scanner.next(); 
+            }
+        }
+        
+                while (true) {
         System.out.print("Digite o segundo número: ");
+
+            if (scanner.hasNextDouble()) {
          num2 = scanner.nextDouble();
+                System.out.println();
+           if (num2 > 0) {
+                    break;
+                } else {
+                
+                    System.out.println("Erro! Apenas valores maiores que 0.");
+                    System.out.println();
+                }
+        
+            } else {
+                                System.out.println();   
+                System.out.println("Erro! Apenas números");
+                                System.out.println();
+                scanner.next(); 
+            }
+        }
+        
+
 
          media = Math.sqrt(num1 * num2);
 
-        System.out.println("A média geométrica é: " + media);
+        System.out.printf("A média geométrica é: %.2f\n" , media);
 
         scanner.close();
     }
