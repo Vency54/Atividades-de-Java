89e294dc26b413fd7a44f1c730d9e2132e7cd9fa
Vency54 <jv028venceslau@gmail.com>
2026-03-24 00:45:16 -0300

    Add input validation for maritime miles conversion

--- TP01Ex08.java
+++ TP01Ex08.java
@@ -1,38 +1,49 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package tp01ex08;
 
-import java.util.*;
+import java.util.Scanner;
 
-/**
- *
- * @author aluno
- */
 public class TP01Ex08 {
 
-    /**
-     * @param args the command line arguments
-     */
+
      /*Nomes: João Victor Lima Venceslau e Rayssa Silva de Oliveira 
+    
      8. Sabendo que uma milha marítima equivale a um mil, oitocentos e cinquenta e
-        dois metros e que um quilômetro possui mil metros, fazer um programa para
-        converter milhas marítimas em quilômetros.*/
+      dois metros e que um quilômetro possui mil metros, fazer um programa para
+      converter milhas marítimas em quilômetros.*/
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+
        Scanner scanner = new Scanner(System.in);
        double milhas;
        double quilometros;
        
+               while (true) {
         System.out.print("Digite a quantidade de milhas marítimas: ");
-        milhas = scanner.nextDouble();
 
+            if (scanner.hasNextDouble()) {
+        milhas = scanner.nextDouble();
+                System.out.println();
+         if (milhas > 0) {
+                    break;
+                } else {
+                
+                    System.out.println("Erro! Apenas valores maiores que 0.");
+                    System.out.println();
+                }
+        
+            } else {
+                                System.out.println();   
+                System.out.println("Erro! Apenas números");
+                                System.out.println();
+                scanner.next(); 
+            }
+        }
+       
+               
         quilometros = milhas * 1.852;
 
-        System.out.println("Equivalente em quilômetros: " + quilometros + " km");
+        System.out.printf("Equivalente em quilômetros: %.2f km \n" , quilometros);
 
+        scanner.close();
     }
     
 }
