b8d4c0896c9b85073199b2f4bfa94f6d9ef775d2
Vency54 <jv028venceslau@gmail.com>
2026-03-24 00:56:48 -0300

    Update TP01Ex10.java

--- TP01Ex10.java
+++ TP01Ex10.java
@@ -1,35 +1,43 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package tp01ex10;
-import java.util.*;
-/**
- *
- * @author aluno
- */
+
+import java.util.Scanner;
+
 public class TP01Ex10 {
 
-    /**
-     * @param args the command line arguments
-     */
     /*Nomes: João Victor Lima Venceslau e Rayssa Silva de Oliveira 
+    
      10. Entrar via teclado com o valor de uma temperatura em graus Celsius, calcular e
          exibir sua temperatura equivalente em Fahrenheit */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+
         Scanner scanner = new Scanner(System.in);
+
         double celsius;
         double fahrenheit;
         
+                                  while (true) {
         System.out.println("Digite a temperatura em celsius: ");
+
+            if (scanner.hasNextDouble()) {
         celsius = scanner.nextDouble();
+                System.out.println();
+                    break;
+        
+            } else {
+                                System.out.println();   
+                System.out.println("Erro! Apenas números");
+                                System.out.println();
+                scanner.next(); 
+            }
+        }
+       
+        
         
         fahrenheit = celsius * 1.8 + 30;
         
-        System.out.println("Conversão para fahrenheit: "+fahrenheit);
+        System.out.printf("Conversão para fahrenheit: %.2f °F \n" , fahrenheit);
         
+        scanner.close();
         
     }
     
