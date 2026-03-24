027738f6e135b5e1df7d9a6f2c0dbdc926c1c900
Vency54 <jv028venceslau@gmail.com>
2026-03-24 00:36:01 -0300

    Implement input validation for sphere diameter
    Added input validation for diameter input.

--- TP01Ex05.java
+++ TP01Ex05.java
@@ -1,38 +1,49 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package tp01ex05;
+
 import java.util.Scanner;
-/**
- *
- * @author aluno
- */
+
 public class TP01Ex05 {
 
-    /**
-     * @param args the command line arguments
-     */
       /* Nomes: João Victor Lima Venceslau e Rayssa Silva de Oliveira 
+    
     5. Calcular e exibir o volume de uma esfera a partir do valor de seu diâmetro que
         será digitado.*/
+    
     public static void main(String[] args) {
-        // TODO code application logic here
-          Scanner scanner = new Scanner(System.in);
+
+        Scanner scanner = new Scanner(System.in);
           double diametro;
           double raio;
           double volume;
           
           
+                 while (true) {
           System.out.println("Digite o diâmetro da esfera: ");
+
+            if (scanner.hasNextDouble()) {
           diametro = scanner.nextDouble();
+                System.out.println();
+                if (diametro > 0) {
+                    break;
+                } else {
+                
+                    System.out.println("Erro! Apenas valores maiores que 0.");
+                    System.out.println();
+                }
+
+            } else {
+                                System.out.println();   
+                System.out.println("Erro! Apenas números");
+                                System.out.println();
+                scanner.next(); 
+            }
+        }
+        
           
           raio = diametro/2;
           
           volume = (4.0 / 3.0) * Math.PI * Math.pow(raio, 3);
           
-          System.out.println("Volume da esfera: "+volume);
+          System.out.printf("Volume da esfera: %.2f\n", volume);
           scanner.close();
     }
     
