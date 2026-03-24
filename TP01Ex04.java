6f4e5c679ece91d703746608f151a844708249b9
Vency54 <jv028venceslau@gmail.com>
2026-03-24 00:31:01 -0300

    Implement input validation for triangle area calculation
    Added input validation for base and height values.

--- TP01Ex04.java
+++ TP01Ex04.java
@@ -1,38 +1,67 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package tp01ex04;
-import java.util.Scanner;
 
-/**
- *
- * @author aluno
- */
+import java.util.*;
+
+
 public class TP01Ex04 {
 
-    /**
-     * @param args the command line arguments
-     */
     /* Nomes: João Victor Lima Venceslau e Rayssa Silva de Oliveira 
+    
     4. A partir dos valores da base e altura de um triângulo, calcular e exibir sua área.*/
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+
         Scanner scanner = new Scanner(System.in);
         double base;
         double altura;
         double area;
         
-        System.out.println("Digite o valor da base:");
-        base = scanner.nextDouble();
+        while (true) {
+            System.out.println("Digite o valor da base:");
+
+            if (scanner.hasNextDouble()) {
+                base = scanner.nextDouble();
+                System.out.println();
+                if (base > 0) {
+                    break;
+                } else {
+                
+                    System.out.println("Erro! Apenas valores maiores que 0.");
+                    System.out.println();
+                }
+
+            } else {
+                                System.out.println();   
+                System.out.println("Erro! Apenas números");
+                                System.out.println();
+                scanner.next(); 
+            }
+        }
         
+             while (true) {
         System.out.println("Digite o valor da altura:");
+
+            if (scanner.hasNextDouble()) {
         altura = scanner.nextDouble();
-        
+                System.out.println();
+                if (altura > 0) {
+                    break;
+                } else {
+                
+                    System.out.println("Erro! Apenas valores maiores que 0.");
+                    System.out.println();
+                }
+
+            } else {
+                                System.out.println();   
+                System.out.println("Erro! Apenas números");
+                                System.out.println();
+                scanner.next(); 
+            }
+        }
+
         area = (base * altura)/2;
         
-        System.out.println("A área do triângulo é: "+area);
+        System.out.printf("A área do triângulo é:%.2f\n", area);
         
         
         scanner.close();
