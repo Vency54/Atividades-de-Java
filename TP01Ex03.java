1eec3a206e28e99b5f46b3c1fdd9b9be59f307ef
Vency54 <jv028venceslau@gmail.com>
2026-03-24 00:29:40 -0300

    Update TP01Ex03.java

--- TP01Ex03.java
+++ TP01Ex03.java
@@ -1,34 +1,47 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package tp01ex03;
+
+
 import java.util.Scanner;
-/**
- *
- * @author aluno
- */
+
 public class TP01Ex03 {
 
-    /**
-     * @param args the command line arguments
-     */
     /* Nomes: João Victor Lima Venceslau e Rayssa Silva de Oliveira 
+    
      3. Calcular e exibir a área de um quadrado a partir do valor de sua diagonal que será
         digitado.*/
+    
     public static void main(String[] args) {
-        // TODO code application logic here
-         Scanner scanner = new Scanner(System.in);
+
+        Scanner scanner = new Scanner(System.in);
          double diagonal;
          double area;
          
-         System.out.println("Digite o valor da diagonal: ");
-         diagonal = scanner.nextDouble();
+
+      while (true) {
+            System.out.println("Digite o valor da diagonal: ");
+
+            if (scanner.hasNextDouble()) {
+                diagonal = scanner.nextDouble();
+                System.out.println();
+                if (diagonal > 0) {
+                    break;
+                } else {
+                
+                    System.out.println("Erro! Apenas valores maiores que 0.");
+                    System.out.println();
+                }
+
+            } else {
+                                System.out.println();   
+                System.out.println("Erro! Apenas números");
+                                System.out.println();
+                scanner.next(); 
+            }
+        }
+
          
          area = (diagonal*diagonal)/2;
          
-         System.out.println("A área do quadrado é: "+area);
+        System.out.printf("A área do quadrado é: %.2f\n", area);
          scanner.close();
          
     }
