7354a6f7fdd721206b43a72ff8bd8a19acdb4b4f
Vency54 <jv028venceslau@gmail.com>
2026-03-25 11:26:17 -0300

    Update TP01Ex12.java

--- TP01Ex12.java
+++ TP01Ex12.java
@@ -1,34 +1,65 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package tp01ex12;
+
 import java.util.*;
-/**
- *
- * @author aluno
- */
+
 public class TP01Ex12 {
 
-    /**
-     * @param args the command line arguments
-     */
     /*Nomes: João Victor Lima Venceslau e Rayssa Silva de Oliveira 
+    
      12. Calcular e exibir o volume de um cone a partir dos valores da altura e do raio da
           base que serão digitados. */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+
         Scanner scanner = new Scanner(System.in);
         double altura;
         double raio;
         double volume; 
         
+        while (true) {
          System.out.println("Digite o valor da altura: ");
+
+            if (scanner.hasNextDouble()) {
          altura = scanner.nextDouble();
+                System.out.println();
+            if (altura > 0) {
+                    break;
+                } else {
+                
+                    System.out.println("Erro! Apenas valores maiores que 0.");
+                    System.out.println();
+                }
+            } else {
+                                System.out.println();   
+                System.out.println("Erro! Apenas números");
+                                System.out.println();
+                scanner.next(); 
+            }
+        }
          
+                 
+                                                   while (true) {
          System.out.println("Digite o valor do raio da base: ");
+
+            if (scanner.hasNextDouble()) {
          raio = scanner.nextDouble();
+                System.out.println();
+            if (raio > 0) {
+                    break;
+                } else {
+                
+                    System.out.println("Erro! Apenas valores maiores que 0.");
+                    System.out.println();
+                }
+            } else {
+                                System.out.println();   
+                System.out.println("Erro! Apenas números");
+                                System.out.println();
+                scanner.next(); 
+            }
+        }
+                                                   
+        
+         
         
         volume = (Math.PI*(Math.pow(raio,2)*altura))/3;
         
