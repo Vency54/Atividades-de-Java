5b14df43862ccbfbccca54a5c55b26768df5b37e
Vency54 <jv028venceslau@gmail.com>
2026-03-25 11:28:19 -0300

    Add input validation for currency conversion

--- TP01Ex15.java
+++ TP01Ex15.java
@@ -1,32 +1,67 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package TP01Ex15;
+
 import java.util.*;
 
-/**
- *
- * @author aluno
- */
+
 public class TP01Ex15 {
 
     /*Nomes: João Victor Lima Venceslau e Rayssa Silva de Oliveira 
+    
    15. Entrar via teclado com o valor da cotação do dólar e uma certa quantidade de
 dólares. Calcular e exibir o valor correspondente em Reais (R$). */
+    
     public static void main(String[] args) {
-                Scanner sc = new Scanner(System.in);
+                Scanner scanner = new Scanner(System.in);
                 
         double CotacaoDolar;
         double Dolares;
         double Reais;
-        
+
+                  
+         while (true) {
         System.out.println("Digite o valor da Cotação do dólar: ");
-        CotacaoDolar = sc.nextDouble();
-        
+
+            if (scanner.hasNextDouble()) {
+        CotacaoDolar = scanner.nextDouble();
+                System.out.println();
+                         if (CotacaoDolar > 0) {
+                    break;
+                } else {
+                
+                    System.out.println("Erro! Cotação Inválida");
+                    System.out.println();
+                }
+            } else {
+                                System.out.println();   
+                System.out.println("Erro! Apenas números");
+                                System.out.println();
+                scanner.next(); 
+            }
+        }
+              
+                     
+                             while (true) {
            System.out.println("Digite uma quantia em dólares: ");
-        Dolares = sc.nextDouble();
+
+            if (scanner.hasNextDouble()) {
+        Dolares = scanner.nextDouble();
+                System.out.println();
+                         if (Dolares >= 0) {
+                    break;
+                } else {
+                
+                    System.out.println("Erro! Valor Inválido");
+                    System.out.println();
+                }
+            } else {
+                                System.out.println();   
+                System.out.println("Erro! Apenas números");
+                                System.out.println();
+                scanner.next(); 
+            }
+        }
+                            
+        
+        
         
         Reais = CotacaoDolar * Dolares;
         
