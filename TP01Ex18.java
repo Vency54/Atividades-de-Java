d92c42f905df120d36fcdd6c3001e715b6582d7e
Vency54 <jv028venceslau@gmail.com>
2026-03-25 11:29:26 -0300

    Refactor product input with validation and error handling

--- TP01Ex18.java
+++ TP01Ex18.java
@@ -1,43 +1,62 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package tp01ex18;
+
 import java.util.*;
-/**
- *
- * @author aluno
- */
+
 public class TP01Ex18 {
 
-    /**
-     * @param args the command line arguments
-     */
     /*Nomes: João Victor Lima Venceslau e Rayssa Silva de Oliveira 
+    
       18. Entrar via teclado com o valor de cinco produtos. Após as entradas, digitar um
            valor referente ao pagamento da somatória destes valores. Calcular e exibir o troco
             que deverá ser devolvido. */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
-            Scanner sc = new Scanner(System.in);
-            double produtos = new double[5];
+            Scanner scanner = new Scanner(System.in);
+            double[] produtos = new double[5];
             double soma = 0;
             double pagamento;
             double troco;
-            
-             for (int i = 0; i < 5; i++) {
+           
+
+            for (int i = 0; i < 5; i++) {
+            while(true){
             System.out.print("Digite o valor do produto " + (i + 1) + ": ");
+            
+            if(!scanner.hasNextDouble()){
+            System.out.println(" ");
+            System.out.println("Erro! Apenas números");
+            System.out.println(" ");
+            scanner.next();
+            continue;
+            }
+            
             produtos[i] = scanner.nextDouble();
+            System.out.println(" ");
+
+           if(produtos[i] < 0){
+            System.out.println("Erro! Apenas números positivos");
+            System.out.println(" ");
+            continue;
+            }
+            
             soma += produtos[i];
-        }            
-            System.out.printf("Total da Compra: R$%.2f%n\"", soma);
+            break;
+        }       
+    }
+            
+            System.out.printf("Total da Compra: R$%.2f%n", soma);
             System.out.println("================================");
             System.out.println("Insira o valor para pagamento: ");
-            pagamento = sc.nextDouble();
+            pagamento = scanner.nextDouble();
+            
+            if(pagamento < soma){
+            System.out.println(" ");
+            System.out.println("Valor insuficiente");
+            return;
+            }
             
             troco = pagamento - soma;
             
+            System.out.println(" ");
             System.out.printf("Valor do troco: R$%.2f%n\"",  troco);
             
     }
