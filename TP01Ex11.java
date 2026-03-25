0765daaacd7c0fd4393ebca63fbf2017a9a0105f
Vency54 <jv028venceslau@gmail.com>
2026-03-25 11:25:59 -0300

    Update TP01Ex11.java

--- TP01Ex11.java
+++ TP01Ex11.java
@@ -1,33 +1,44 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package tp01ex11;
+
 import java.util.*;
-/**
- *
- * @author aluno
- */
+
 public class TP01Ex11 {
 
-    /**
-     * @param args the command line arguments
-     */
     /*Nomes: João Victor Lima Venceslau e Rayssa Silva de Oliveira 
+    
      11. A partir do diâmetro de um círculo que será digitado, calcular e exibir sua área. */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+
         Scanner scanner = new Scanner(System.in);
          double diametro;
          double area;
          
+         while (true) {
          System.out.println("Digite o valor do diâmetro: ");
+
+            if (scanner.hasNextDouble()) {
          diametro = scanner.nextDouble();
+                System.out.println();
+            if (diametro > 0) {
+                    break;
+                } else {
+                
+                    System.out.println("Erro! Apenas valores maiores que 0.");
+                    System.out.println();
+                }
+            } else {
+                                System.out.println();   
+                System.out.println("Erro! Apenas números");
+                                System.out.println();
+                scanner.next(); 
+            }
+        }
+         
          
          area = Math.PI*(Math.pow((diametro/2),2));
          
-         System.out.println("Valor da área do círculo: "+area);
+         System.out.printf("Valor da área do círculo: %.2f\n" , area);
+         scanner.close();
     }
     
 }
